d4c743a67d83e646122beb8c482c6eb293648561
jamie7dev <jamie7dev@gmail.com>
2022-11-03 15:10:48 +0900

    add: pictures, background.js

---
+++ js/background.js
@@ -0,0 +1,19 @@
+const images = [
+  "00.jpg",
+  "01.jpg",
+  "02.jpg",
+  "03.jpg",
+  "04.jpg",
+  "05.jpg",
+  "06.jpg",
+  "07.jpg"
+]
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `/img/${chosenImage}`;
+// console.log(bgImage);
+
+document.body.appendChild(bgImage);
