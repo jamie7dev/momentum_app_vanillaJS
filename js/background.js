cc20dfc4278c96d6c490d2dcdcc1c46fbe5e9145
jamie7dev <jamie7dev@gmail.com>
2022-11-06 22:17:45 +0900

    add: bg image src

--- js/background.js
+++ js/background.js
@@ -15,7 +15,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `./img/${chosenImage}`;
+bgImage.src = `img/${chosenImage}`;
 // console.log(bgImage);
 
 document.body.appendChild(bgImage);
