a80586007a495c22f35b4c558c07d5b9c2a04397
jamie7dev <jamie7dev@gmail.com>
2022-11-06 22:34:39 +0900

    키워드 - 기능, 단위 #issue번호
    변경사항 :

--- js/background.js
+++ js/background.js
@@ -15,7 +15,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `../images/${chosenImage}`;
+bgImage.src = `../img/${chosenImage}`;
 // console.log(bgImage);
 
 document.body.appendChild(bgImage);
